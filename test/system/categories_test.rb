51cff566ad40560373c7b465b92dc4614040601a
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-06-13 20:14:23 +0800

    apply categories system test

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -1,41 +1,53 @@
 require "application_system_test_case"
 
 class CategoriesTest < ApplicationSystemTestCase
-  setup do
-    @category = categories(:one)
-  end
-
   test "visiting the index" do
+    sign_in users(:melissa)
     visit categories_url
     assert_selector "h1", text: "Categories"
   end
 
   test "creating a Category" do
+    sign_in users(:melissa)
     visit categories_url
     click_on "New Category"
 
-    fill_in "Category", with: @category.category
+    fill_in "Category", with: "Hello Text"
     click_on "Create Category"
 
     assert_text "Category was successfully created"
-    click_on "Back"
   end
 
   test "updating a Category" do
+    sign_in users(:melissa)
+    visit categories_url
+    click_on "New Category"
+
+    fill_in "Category", with: "Hello Text"
+    click_on "Create Category"
+    assert_text "Category was successfully created"
+    
     visit categories_url
     click_on "Edit", match: :first
 
-    fill_in "Category", with: @category.category
+    fill_in "Category", with: "Hello Test"
     click_on "Update Category"
 
     assert_text "Category was successfully updated"
-    click_on "Back"
   end
 
   test "destroying a Category" do
+    sign_in users(:melissa)
+    visit categories_url
+    click_on "New Category"
+
+    fill_in "Category", with: "Hello Text"
+    click_on "Create Category"
+    assert_text "Category was successfully created"
+
     visit categories_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on "Delete", match: :first
     end
 
     assert_text "Category was successfully destroyed"
