e26f2ef9502cf9631ecc38fd6032eb037753123e
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-06-15 19:45:49 +0800

    updates category models test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -3,12 +3,14 @@ require "test_helper"
 class CategoryTest < ActiveSupport::TestCase
   test "valid category" do
     category = Category.new(category: 'Life')
+    category.save
     category.valid?
     assert_empty category.errors[:category]
   end
 
   test "invalid if category is empty" do
-    category = Category.new(category: '')
+    category = Category.new(category: nil)
+    category.save
     assert_not category.valid?
   end
 
@@ -16,6 +18,7 @@ class CategoryTest < ActiveSupport::TestCase
     category = categories(:one)
     category_two = categories(:two)
     category.category = category_two.category
+    category.save
     assert_not category.valid?
   end
 end
