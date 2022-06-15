f1e2e3bce7e24ee0941d4f0f7495c9eeb8c66693
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-06-15 20:21:41 +0800

    adds model testing for task validations

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid task" do
+    task = Task.new(title: 'Task 1', details: 'asdf', completed: true);
+    task.save
+    task.valid?
+    assert_empty task.errors[:task]
+  end
+
+  test "valid task with title only" do
+    task = Task.new(title: 'Task 1', details: nil, completed: false);
+    task.save
+    task.valid?
+    assert_empty task.errors[:task]
+  end
+
+  test "invalid task if there is no title" do
+    task = Task.new(title: nil, details: nil, completed: false)
+    task.save
+    assert_not task.valid?
+  end
 end
