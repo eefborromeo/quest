0f7f262a310bfadcbdd095ffe46aff47787f1933
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-06-15 11:02:04 +0800

    creates unit tests for category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid category" do
+    category = Category.new(category: 'Life')
+    category.valid?
+    assert_empty category.errors[:category]
+  end
+
+  test "invalid if category is empty" do
+    category = Category.new(category: '')
+    assert_not category.valid?
+  end
+
+  test "invalid if category already exists" do
+    category = categories(:one)
+    category_two = categories(:two)
+    category.category = category_two.category
+    assert_not category.valid?
+  end
 end
