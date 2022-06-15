da57d37826d70c584bc2b7db6509c6a5c17da46d
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-06-15 19:44:30 +0800

    updates Date.today -> Time.zone

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,10 +10,10 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   def show
     @no_date = @category.tasks.where(:date => nil, :completed => false)
-    @upcoming = @category.tasks.where.not(:date => Date.today).where.not('date < ?', Date.today).where('date > ?', Date.today.end_of_week).where(:completed => false)
-    @this_week = @category.tasks.where.not(:date => Date.today).where.not('date < ?', Date.today).where(:date => Date.today.beginning_of_week...Date.today.end_of_week, :completed => false)
-    @today = @category.tasks.where(:date => Date.today, :completed => false)
-    @overdue = @category.tasks.where('date < ?', Date.today).where(:completed => false)
+    @upcoming = @category.tasks.where.not(:date => Time.zone.today).where.not('date < ?', Time.zone.today).where('date > ?', Time.zone.today.end_of_week).where(:completed => false)
+    @this_week = @category.tasks.where.not(:date => Time.zone.today).where.not('date < ?', Time.zone.today).where(:date => Time.zone.today.beginning_of_week...Time.zone.today.end_of_week, :completed => false)
+    @today = @category.tasks.where(:date => Time.zone.today, :completed => false)
+    @overdue = @category.tasks.where('date < ?', Time.zone.today).where(:completed => false)
     @done = @category.tasks.where(:completed => true)
   end
 
