ae4b75b8c7b7c57d987f01d4f33a9cc635c138ba
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-06-24 21:27:31 +0800

    updates tests to use fixtures

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -1,47 +1,47 @@
 require "application_system_test_case"
 
 class CategoriesTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  setup do
     sign_in users(:melissa)
+  end 
+
+  test "visiting the index" do
     visit categories_url
     assert_selector "h1", text: "Categories"
   end
 
   test "creating a Category" do
-    sign_in users(:melissa)
     visit categories_url
     click_on "New Category"
 
-    fill_in "Category", with: "Hello Text"
+    fill_in "Category", with: categories(:one).category
     click_on "Create Category"
 
     assert_text "Category was successfully created"
   end
 
   test "updating a Category" do
-    sign_in users(:melissa)
     visit categories_url
     click_on "New Category"
 
-    fill_in "Category", with: "Hello Text"
+    fill_in "Category", with: categories(:one).category
     click_on "Create Category"
     assert_text "Category was successfully created"
     
     visit categories_url
     click_on "Edit", match: :first
 
-    fill_in "Category", with: "Hello Test"
+    fill_in "Category", with: categories(:two).category
     click_on "Update Category"
 
     assert_text "Category was successfully updated"
   end
 
   test "destroying a Category" do
-    sign_in users(:melissa)
     visit categories_url
     click_on "New Category"
 
-    fill_in "Category", with: "Hello Text"
+    fill_in "Category", with: categories(:one).category
     click_on "Create Category"
     assert_text "Category was successfully created"
 
--- test/system/tasks_test.rb
+++ test/system/tasks_test.rb
@@ -1,63 +1,63 @@
 require "application_system_test_case"
 
 class TasksTest < ApplicationSystemTestCase
-    test "creating a task" do
+    setup do
         sign_in users(:melissa)
+    end
+    test "creating a task" do
         visit categories_url
         click_on "New Category"
 
-        fill_in "Category", with: "Hello Text"
+        fill_in "Category", with: categories(:one).category
         click_on "Create Category"
 
         assert_text "Category was successfully created"
 
         click_on "Create a new task"
-        fill_in "Title", with: "Task 1"
-        fill_in "Details", with: "asdfasd"
+        fill_in "Title", with: tasks(:one).title
+        fill_in "Details", with: tasks(:one).details
         click_on "Create Task"
 
         assert_text "Task was successfully created."
     end
 
     test "updating a task" do
-        sign_in users(:melissa)
         visit categories_url
         click_on "New Category"
 
-        fill_in "Category", with: "Hello Text"
+        fill_in "Category", with: categories(:one).category
         click_on "Create Category"
 
         assert_text "Category was successfully created"
 
         click_on "Create a new task"
-        fill_in "Title", with: "Task 1"
-        fill_in "Details", with: "asdfasd"
+        fill_in "Title", with: tasks(:one).title
+        fill_in "Details", with: tasks(:one).details
         click_on "Create Task"
-
         assert_text "Task was successfully created."
-        click_on "Task 1"
+        
+        click_on tasks(:one).title
         click_on "Edit"
-        fill_in "Title", with: "Task 2"
+        fill_in "Title", with: tasks(:two).title
         click_on "Update Task"
     end
 
     test "deleting a task" do
-        sign_in users(:melissa)
         visit categories_url
         click_on "New Category"
 
-        fill_in "Category", with: "Hello Text"
+        fill_in "Category", with: categories(:one).category
         click_on "Create Category"
 
         assert_text "Category was successfully created"
 
         click_on "Create a new task"
-        fill_in "Title", with: "Task 1"
-        fill_in "Details", with: "asdfasd"
+        fill_in "Title", with: tasks(:one).title
+        fill_in "Details", with: tasks(:one).title
         click_on "Create Task"
-
         assert_text "Task was successfully created."
-        click_on "Task 1"
+
+        click_on tasks(:one).title
         page.accept_confirm do 
             click_on "Delete", match: :first
         end
